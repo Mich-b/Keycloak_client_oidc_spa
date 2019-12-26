d8c50e063ce3d0ab25b89a41f25ff7dc47336ae3
portasecura <mboeynaems@outlook.com>
2019-12-26 15:50:17 +0100

    modified urls

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,7 @@
 export class Constants {
-  public static stsAuthority = 'http://localhost/auth/realms/master/';
+  public static stsAuthority = 'https://keycloak.michaelboeynaems.com/auth/realms/WebAuthnTest/';
   public static clientId = 'oidc-spa';
-  public static clientRoot = 'http://localhost:4200/';
-  //public static clientRoot = 'https://gracious-kare-74625e.netlify.com/';
+  public static clientRoot = 'https://demo.michaelboeynaems.com/';
   public static clientScope = 'openid profile email';
   public static apiAudience = 'https://localapi.internal';
   public static apiRoot = 'https://localapi.internal/api/';
